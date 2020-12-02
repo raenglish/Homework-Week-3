868b0fa829a5655f52925bdf8dbff13802bf3844
rae <email@email.email>
2020-12-01 21:29:58 -0500

    initial commit

---
+++ RachaelHomeworkWeek3/Cat.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RachaelHomeworkWeek3
+{
+    class Cat
+    {
+        public string FirstName
+        {
+            get; set;
+        }
+        public string LastName
+        {
+            get; set;
+        }
+
+        public Cat(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string getCatName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public int acceptedNumberOfPets()
+        {
+            var desiredPets = new Random();
+            return desiredPets.Next(1, 21);
+        }
+
+        public void petTheCat()
+        {
+            int x = 0;
+            bool enough = false;
+            do
+            {
+                Console.WriteLine($"Would you like to pet {getCatName()}? (yes/no)");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    
+                    if (x <= acceptedNumberOfPets())
+                    {
+                        Console.WriteLine($"{getCatName()} really seems to be enjoying that.");
+                        x++;
+                        Console.WriteLine($"You have pet {getCatName()} {x} times");
+                    }
+                    else if (x > acceptedNumberOfPets())
+                    {
+                        Console.WriteLine($"OH SHIT BRO, I don't think {getCatName()} likes that very much!!");
+                        Console.WriteLine("*Your hand has been ripped to shreds and you're bleeding all over the carpet*");
+                        Console.WriteLine("You might want to get that checked out...");
+                        enough = true;
+                    }
+
+                }
+                else if (answer == "no" && x == acceptedNumberOfPets())
+                {
+                    Console.WriteLine($"{getCatName()} seemed to really enjoy that. You're the best!");
+                    enough = true;
+                }
+                else if (answer == "no" && x < acceptedNumberOfPets())
+                {
+                    Console.WriteLine($"{getCatName()} seems upset that you stopped petting them... maybe you should go apologize.");
+                    enough = true;
+                }
+
+            }
+            while (enough == false);
+        }
+    }
+
+}
---
+++ RachaelHomeworkWeek3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RachaelHomeworkWeek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your Cat's first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your cat's last name?");
+            string lastName = Console.ReadLine();
+            Cat yourCat = new Cat(firstName, lastName);
+            Console.WriteLine($"I hope you know how many pets {yourCat.getCatName()} enjoys! Let's play a game!");
+            yourCat.petTheCat();
+
+        }
+    }
+}
